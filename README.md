4ae4e1e4d6f9b81f15f0b7641c22ea8a92583c27
hassanyakef <yhassan@vt.edu>
2019-12-11 12:09:32 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Eureka
+An idea-generator where people (general public) can post an app idea.
+
+### How to run this locally?
+- ``` npm install ```
+- ```npm run client-install```
+- ```npm run dev```
+
+### keys_dev.js needs to be created in the config folder:
+```
+module.exports = {
+   mongoURI: 'CHANGE_ME',
+   secretOrKey: 'secret'
+}
+```
+
---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+// Checks if request includes a JWT token,
+// if it does, decode the token and pass the user id to the req.user object
+module.exports = (req, res, next) => {
+  // Get token from header
+  const token = req.header('x-auth-token');
+  // Check if no token
+  if (!token)
+    return res.status(401).json({message: 'No token, authorization denied'});
+
+  // Varify token
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    req.user = decoded.user;
+    next();
+
+  } catch (err) {
+    res.status(401).json({msg: 'token is not valid'});
+  }
+};
