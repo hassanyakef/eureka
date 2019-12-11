ddbd41c819620444a91bb0ea60bbedf103f4558b
hassanyakef <yhassan@vt.edu>
2019-12-11 13:21:03 -0500

    add routes

---
+++ routes/api/ideas.js
@@ -0,0 +1,234 @@
+const express = require('express');
+const router = express.Router();
+const Profile = mongoose.model('profile');
+const User = mongoose.model('users');
+const Idea = mongoose.model('ideas');
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
+
+/**
+ * @route POST api/ideas
+ * @desc Create an idea
+ * @access Private
+ */
+router.post('/', [auth, [check('text', 'Text is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const newIdea = new Idea({
+        text: req.body.text, name: user.name, avatar: user.avatar, user: req.user.id
+      });
+
+      const idea = await newIdea.save();
+      return res.json(idea);
+
+    } catch (err) {
+      console.log(err.message);
+      return res.status(500).send('Server Error');
+    }
+
+  });
+
+/**
+ * @route GET api/ideas
+ * @desc Get all ideas
+ * @access Private
+ */
+router.get('/', auth, async (req, res) => {
+  try {
+    const ideas = await Idea.find().sort({ date: -1 });
+    return res.json(ideas);
+
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).send('Server Error');
+  }
+});
+
+/**
+ * @route GET api/ideas/:id
+ * @desc Get a single idea by id
+ * @access Private
+ */
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const idea = await Idea.findById(req.params.id);
+    if (!idea) {
+      return res.status(404).json({ msg: 'Idea not found' });
+    }
+    return res.json(idea);
+
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Idea not found' });
+    }
+    return res.status(500).send('Server Error');
+  }
+});
+
+/**
+ * @route DELETE api/ideas/:id
+ * @desc Delete a single idea by id
+ * @access Private
+ */
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const idea = await Idea.findById(req.params.id);
+    if (!idea) {
+      return res.status(404).json({ msg: 'Idea not found' });
+    }
+
+    // Check user
+    if (idea.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+    await idea.remove();
+
+    return res.json({ msg: 'Idea removed' });
+
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Idea not found' });
+    }
+    return res.status(500).send('Server Error');
+  }
+});
+
+/**
+ * @route PUT api/ideas/like/:id
+ * @desc Like a idea
+ * @access Private
+ */
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const idea = await Idea.findById(req.params.id);
+    if (!idea) {
+      return res.status(404).json({ msg: 'Idea not found' });
+    }
+    // Check if the idea has already been liked
+    if (idea.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+      return res.status(400).json({ msg: 'Idea already liked' });
+    }
+    idea.likes.unshift(req.user.id);
+    await idea.save();
+
+    res.json(idea.likes);
+
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Idea not found' });
+    }
+    return res.status(500).send('Server Error');
+  }
+});
+
+/**
+ * @route PUT api/ideas/unlike/:id
+ * @desc Unlike a idea
+ * @access Private
+ */
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const idea = await Idea.findById(req.params.id);
+    if (!idea) {
+      return res.status(404).json({ msg: 'Idea not found' });
+    }
+    // Check if the idea has already been liked
+    if (idea.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+      return res.status(400).json({ msg: 'Idea has not yet been liked' });
+    }
+    // Get remove idx
+    const removeIdx = idea.likes.map(like => like.user.toString()).indexOf(req.user.id);
+    idea.likes.splice(removeIdx, 1);
+    await idea.save();
+
+    res.json(idea.likes);
+
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Idea not found' });
+    }
+    return res.status(500).send('Server Error');
+  }
+});
+
+/**
+ * @route POST api/ideas/comment/:id
+ * @desc Comment on a idea
+ * @access Private
+ */
+router.post('/comment/:id', [auth, [check('text', 'Text is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const idea = await Idea.findById(req.params.id);
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      };
+
+      idea.comments.unshift(newComment);
+
+      await idea.save();
+      return res.json(idea.comments);
+
+    } catch (err) {
+      console.log(err.message);
+      return res.status(500).send('Server Error');
+    }
+
+  });
+
+/**
+ * @route DELETE api/ideas/comment/:id/:comment_id
+ * @desc Remove a comment from a idea
+ * @access Private
+ */
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    // const user = await User.findById(req.user.id).select('-password');
+    const idea = await Idea.findById(req.params.id);
+    // Pull out comment from the idea
+    const comment = idea.comments.find(comment => comment.id === req.params.comment_id);
+
+    // Make sure comment exist
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    // Get remove index
+    const removeIdx = idea.comments.map(comment => comment.user.toString())
+      .indexOf(req.user.id);
+    idea.comments.splice(removeIdx, 1);
+
+    await idea.save();
+    return res.json(idea.comments);
+
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).send('Server Error');
+  }
+
+});
+module.exports = router;
---
+++ routes/api/profile.js
@@ -0,0 +1,139 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = mongoose.model('profile');
+const User = mongoose.model('users');
+const Idea = mongoose.model('ideas');
+const { check, validationResult } = require('express-validator/check');
+
+/**
+ * @route GET api/profile/me
+ * @desc Get current user's profile
+ * @access Private
+ */
+router.get('/me', auth, async (req, res) => {
+  try {
+    let profile = await Profile.findOne({ user: req.user.id })
+      .populate('user', ['name', 'avatar']);
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+    return res.json(profile);
+
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send('Server error');
+  }
+});
+
+/**
+ * @route POST api/profile/
+ * @desc Create or update a user profile
+ * @access Private
+ */
+router.post('/', auth, async (req, res) => {
+
+  const {
+    website, location, bio, status, githubUsername, interests, twitter, facebook, linkedin, instagram
+  } = req.body;
+
+  const profileFields = {};
+
+  profileFields.user = req.user.id;
+
+  if (website) profileFields.company = website;
+  if (location) profileFields.location = location;
+  if (bio) profileFields.bio = bio;
+  if (status) profileFields.status = status;
+  if (githubUsername) profileFields.githubUsername = githubUsername;
+
+  if (interests) {
+    profileFields.interests = interests.split(',').map(interest => interest.trim());
+  }
+
+  profileFields.social = {};
+
+  if (twitter) profileFields.social.twitter = twitter;
+  if (facebook) profileFields.social.facebook = facebook;
+  if (linkedin) profileFields.social.linkedin = linkedin;
+  if (instagram) profileFields.social.instagram = instagram;
+
+  try {
+    let profile = await Profile.findOne({ user: req.user.id });
+    if (profile) {
+      // Update profile
+      profile =
+        await Profile.findOneAndUpdate({ user: req.user.id }, { $set: profileFields },
+          { new: true });
+      return res.json(profile);
+    }
+
+    // Create profile
+    profile = new Profile(profileFields);
+    await profile.save();
+    return res.json(profile);
+
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send('Server Error');
+  }
+});
+
+/**
+ * @route GET api/profile
+ * @desc Get all profiles
+ * @access Public
+ */
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find({}).populate('user', ['name', 'avatar']);
+    return res.json(profiles);
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).send('Server Error');
+  }
+});
+
+/**
+ * @route GET api/profile/user/:user_id
+ * @desc Get user's profile
+ * @access Public
+ */
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.params.user_id })
+      .populate('user', ['name', 'avatar']);
+    if (!profile) {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    return res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    return res.status(500).send('Server Error');
+  }
+});
+
+/**
+ * @route DELETE api/profile
+ * @desc Delete profile, user, and post.
+ * @access Private
+ */
+router.delete('/', auth, async (req, res) => {
+  try {
+    // Remove user posts
+    await Idea.deleteMany({ user: req.user.id });
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+    return res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).send('Server Error');
+  }
+});
+
+module.exports = router;
